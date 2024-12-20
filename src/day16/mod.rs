425fe92e25346af785df6c247fe7225bfb175f05
Wojciech Falba <wojciech.falba@gmail.com>
2024-12-20 10:20:05 +0100

    day 16

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -4,6 +4,11 @@ use std::{collections::HashMap, fs};
 
 pub fn answer() {
     let file = fs::read_to_string("day16/input.txt").unwrap();
+    pt1(&file);
+    pt2(&file);
+}
+
+fn pt1(file: &str) {
     let mut looked_aunt = HashMap::new();
     looked_aunt.insert("children", "3");
     looked_aunt.insert("cats", "7");
@@ -31,7 +36,57 @@ pub fn answer() {
     });
     for (i, aunt) in aunts.enumerate() {
         if aunt.iter().all(|(&key, &val)| *looked_aunt[key] == *val) {
-            println!("{} {:?}", i + 1, aunt)
+            println!("Number of sue: {} {:?}", i + 1, aunt)
+        }
+    }
+}
+
+fn pt2(file: &str) {
+    let mut looked_aunt = HashMap::new();
+    looked_aunt.insert("children", 3);
+    looked_aunt.insert("cats", 7);
+    looked_aunt.insert("samoyeds", 2);
+    looked_aunt.insert("pomeranians", 3);
+    looked_aunt.insert("akitas", 0);
+    looked_aunt.insert("vizslas", 0);
+    looked_aunt.insert("goldfish", 5);
+    looked_aunt.insert("trees", 3);
+    looked_aunt.insert("cars", 2);
+    looked_aunt.insert("perfumes", 1);
+
+    let mut mfcsam: HashMap<&str, Box<dyn Fn(i32, i32) -> bool>> = HashMap::new();
+
+    mfcsam.insert("children", Box::new(|x, y| x == y));
+    mfcsam.insert("cats", Box::new(|a, b| a < b));
+    mfcsam.insert("samoyeds", Box::new(|a, b| a == b));
+    mfcsam.insert("pomeranians", Box::new(|a, b| a > b));
+    mfcsam.insert("akitas", Box::new(|a, b| a == b));
+    mfcsam.insert("vizslas", Box::new(|a, b| a == b));
+    mfcsam.insert("goldfish", Box::new(|a, b| a > b));
+    mfcsam.insert("trees", Box::new(|a, b| a < b));
+    mfcsam.insert("cars", Box::new(|a, b| a == b));
+    mfcsam.insert("perfumes", Box::new(|a, b| a == b));
+
+    let aunts = file.lines().map(|line| {
+        let s = line
+            .split_once(": ")
+            .unwrap()
+            .1
+            .split(", ")
+            .map(|s| s.split(": ").collect::<Vec<&str>>())
+            .collect::<Vec<Vec<&str>>>();
+        let mut hs = HashMap::new();
+        s.iter().for_each(|v| {
+            hs.insert(v[0], v[1].parse::<i32>().unwrap());
+        });
+        hs
+    });
+    for (i, aunt) in aunts.enumerate() {
+        if aunt
+            .iter()
+            .all(|(&key, &val)| mfcsam[key](looked_aunt[key], val))
+        {
+            println!("Number of sue: {} {:?}", i + 1, aunt)
         }
     }
 }
